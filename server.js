3eb1114e9933c55583e8ded7d2db9a4622cd2ab0
cjrabe17 <cjrabe17@gmail.com>
2018-03-28 22:05:26 -0400

    Added heroku info to server.js

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 app.listen(PORT, function() {
     console.log("Listening on port: " + PORT);
 });
